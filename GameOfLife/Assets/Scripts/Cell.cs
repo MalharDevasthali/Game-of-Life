ade962c34493c4829b85c1fa8a73b6fe6d7960bc
Malhar Devasthali <malhar@deftouch.co>
2021-01-02 15:30:01 +0530

    #Added Grid

---
+++ GameOfLife/Assets/Scripts/Cell.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    private int vaule;
+    public int GetCellValue()
+    {
+        return vaule;
+    }
+}
---
+++ GameOfLife/Assets/Scripts/GridHolder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridHolder : MonoBehaviour
+{
+
+    public Cell Cell;
+    [SerializeField] private int rows;
+    [SerializeField] private int cols;
+    private void Start()
+    {
+        GenerateGrid();
+    }
+    private void GenerateGrid()
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Transform cell = Instantiate(Cell.transform);
+                cell.SetParent(this.transform);
+            }
+        }
+    }
+}
+
+
+
+
+
